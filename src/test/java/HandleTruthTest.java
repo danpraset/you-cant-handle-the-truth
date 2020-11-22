9598db3411eaf7217b0ba182c71b78b294a67db1
danpraset <danpraset@gmail.com>
2020-11-21 22:25:29 -0800

    test case 2 and 3 done

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -26,10 +26,32 @@ public class HandleTruthTest {
         Set<String> set0 = new HashSet<>(Arrays.asList("hello"));
         test1.put(1, set0);
 
+        //test case 2
+        TreeMap<Integer, Set<String>> test2 = new TreeMap<>(Collections.reverseOrder());
+
+        Set<String> set1 = new HashSet<>(Arrays.asList("my", "daniel", "is", "name", "thats", "it", "prasetyo"));
+        test2.put(1, set1);
+
+        Set<String> set2 = new HashSet<>(Arrays.asList("and"));
+        test2.put(2, set2);
+
+        //test case 3
+        TreeMap<Integer, Set<String>> test3 = new TreeMap<>(Collections.reverseOrder());
+
+        Set<String> set4 = new HashSet<>(Arrays.asList("i", "washington", "you", "oregon"));
+        test3.put(1, set4);
+
+        Set<String> set3 = new HashSet<>(Arrays.asList("live", "in"));
+        test3.put(2, set3);
+
+
+
 
 
         TreeMap<Integer, Set<String>>[] expectedCases = new TreeMap[6];
         expectedCases[0] = test1;
+        expectedCases[1] = test2;
+        expectedCases[2] = test3;
 
 
 
