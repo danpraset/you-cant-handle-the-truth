d0cc3249757fd74ffd8729e711eddae6d199ba79
danpraset <danpraset@gmail.com>
2020-11-21 22:23:33 -0800

    test case 1

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import java.util.*;
 
+import static org.junit.Assert.assertEquals;
+
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
@@ -18,5 +20,25 @@ public class HandleTruthTest {
                 "extra dip dip sauce sauce sauce coding six six six six"
         };
 
+        //test case 1
+        TreeMap<Integer, Set<String>> test1 = new TreeMap<>(Collections.reverseOrder());
+
+        Set<String> set0 = new HashSet<>(Arrays.asList("hello"));
+        test1.put(1, set0);
+
+
+
+        TreeMap<Integer, Set<String>>[] expectedCases = new TreeMap[6];
+        expectedCases[0] = test1;
+
+
+
+        for (int i = 0; i < testCases.length; i++) {
+            TreeMap<Integer, Set<String>> actual = HandleTruth.wordCount(testCases[i]);
+            TreeMap<Integer, Set<String>> expected = expectedCases[i];
+            assertEquals(actual, expected);
+        }
     }
+
+}
 }
