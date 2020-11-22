71ed96e1516852e3e72bf3496a0ddeedec927bc8
danpraset <danpraset@gmail.com>
2020-11-21 22:21:13 -0800

    set up testcases

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,8 +1,22 @@
 import org.junit.Test;
+import java.util.*;
 
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-        HandleTruth.wordCount();
+        //HandleTruth.wordCount();
+    }
+
+    @Test
+    public void realTest() {
+        String[] testCases = {
+                "hello",
+                "my name is daniel prasetyo and and thats it",
+                "i live in washington you live in oregon",
+                "yes yes yes yes no no no maybe",
+                "java java html html c# c# javascript html",
+                "extra dip dip sauce sauce sauce coding six six six six"
+        };
+
     }
 }
